b6869e68b4652d696e1973f33dec717cdbebd175
Justin Blanco <blancorjustin@gmail.com>
2021-10-10 22:18:14 -0700

    Added routes for followers

--- api/users_api.py
+++ api/users_api.py
@@ -63,7 +63,7 @@ def retrive_user(response, id: hug.types.number, db: sqlite):
   return {"users": users}
 
 @hug.get(
-  "/search",
+  "/users/search",
   examples=[
     "username=jblanco",
     "email=jrblanco@csu.fullerton.edu",
@@ -76,10 +76,6 @@ def search(request, db: sqlite, logger: log):
   conditions = []
   values = []
 
-  if "username" in request.params:
-    conditions.append("username = ?")
-    values.append(request.params["username"])
-
   for column in ["username", "email", "bio"]:
     if column in request.params:
       conditions.append(f"{column} LIKE ?")
@@ -91,3 +87,69 @@ def search(request, db: sqlite, logger: log):
     return {"users": users.rows_where(where, values)}
   else:
     return {"users": users.rows}
+
+@hug.get("/followers/")
+def followers(db: sqlite):
+  return {"followers": db["followers"].rows}
+
+@hug.post("/followers/", status=hug.falcon.HTTP_201)
+def create_follower(
+  response,
+  follower_id: hug.types.number,
+  following_id: hug.types.number,
+  db: sqlite,
+):
+  followers = db["followers"]
+
+  follower = {
+    "follower_id": follower_id,
+    "following_id": following_id,
+  }
+
+  try:
+    followers.insert(follower)
+    follower["id"] = followers.last_pk
+  except Exception as e:
+    response.status = hug.falcon.HTTP_409
+    return {"error": str(e)}
+
+  response.set_header("Location", f"/followers/{follower['id']}")
+  return follower
+
+@hug.get("/followers/{id}")
+def retrive_follower(response, id: hug.types.number, db: sqlite):
+  followers = []
+  try:
+    followers = db["followers"].get(id)
+    followers.append()
+  except sqlite_utils.db.NotFoundError:
+    response.status = hug.falcon.HTTP_404
+  return {"followers": followers}
+
+@hug.get(
+  "/followers/search",
+  examples=[
+    "follower_id=1",
+    "following_id=3",
+  ],
+)
+def search(request, db: sqlite, logger: log):
+  followers = db["followers"]
+
+  conditions = []
+  values = []
+
+  if "follower_id" in request.params:
+    conditions.append("follower_id = ?")
+    values.append(request.params["follower_id"])
+
+  if "following_id" in request.params:
+    conditions.append("following_id = ?")
+    values.append(request.params["following_id"])
+
+  if conditions:
+    where = "AND ".join(conditions)
+    logger.debug('WHERE "%s", %r', where, values)
+    return {"followers": followers.rows_where(where, values)}
+  else:
+    return {"followers": followers.rows}
