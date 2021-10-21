338a532c42595326a0cd2d8be5a8ccd5971a55dd
Justin Blanco <blancorjustin@gmail.com>
2021-10-20 18:13:42 -0700

    Added reposts join table

--- api/timelines.sql
+++ api/timelines.sql
@@ -7,6 +7,12 @@ CREATE TABLE IF NOT EXISTS posts (
     timestamp   INTEGER DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS reposts (
+    id          INTEGER PRIMARY KEY,
+    original_id INTEGER NOT NULL,
+    repost_id   INTEGER NOT NULL
+);
+
 CREATE INDEX IF NOT EXISTS post_username_idx ON posts(username);
 CREATE INDEX IF NOT EXISTS post_timestamp_idx ON posts(timestamp);
 
