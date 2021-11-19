8abd335d90a33b59d8e696d2200eaa2064c1182d
Justin Blanco <blancorjustin@gmail.com>
2021-11-18 21:08:30 -0800

    added likes-backend to haproxy

--- api/likes_api.py
+++ api/likes_api.py
@@ -12,6 +12,7 @@ import sqlite_utils
 config = configparser.ConfigParser()
 config.read("./etc/posts_api.ini")
 logging.config.fileConfig(config["logging"]["config"], disable_existing_loggers=False)
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 # Arguements to inject into route functions
 @hug.directive()
@@ -27,12 +28,21 @@ def log(name=__name__, **kwargs):
 #
 # Get the number of likes of a post
 @hug.get("/likes/{id}")
-def get_likes(request, db: sqlite, id: hug.types.number):
+def get_likes(response, db: sqlite, id: hug.types.number):
   # get the users this user is following
-  posts = []
+  # r = redis.Redis(host='localhost', port=6379, db=0)
+  if not r.exists(f"posts:{id}:likes"):
+    response.status = hug.falcon.HTTP_404
+  return {"likes": r.get(f"posts:{id}:likes")}
 
-# Like a post
+# Like a post by a certain amount
 @hug.put("/likes/{id}", status=hug.falcon.HTTP_201)
-def add_likes(response, id: hug.types.number):
-  r = redis.Redis(host='localhost', port=6379, db=0)
-  r.incr(f'posts:{id}:likes', 1)
+def add_likes(
+  response, 
+  id: hug.types.number,
+  amount: hug.types.number
+  ):
+  # r = redis.Redis(host='localhost', port=6379, db=0)
+  r.incr(f'posts:{id}:likes', amount)
+
+# list of users
