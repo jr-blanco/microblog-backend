b3e6cb43ee5da30a51b02e7a72efb624cbbf25f6
Justin Blanco <blancorjustin@gmail.com>
2021-12-16 16:35:14 -0800

    modified like endpoint to insert into jobs queue

--- api/likes_api.py
+++ api/likes_api.py
@@ -1,7 +1,8 @@
 import configparser
+import json
 import logging.config
 import redis
-
+import greenstalk
 import hug
 
 
@@ -14,6 +15,10 @@ logging.config.fileConfig(config["logging"]["config"], disable_existing_loggers=
 @hug.directive()
 def _redis(dbPort=6379, dbValue=0, **kwargs):
   return redis.Redis(host='localhost', port=dbPort, db=dbValue)
+
+@hug.directive()
+def jobs(url='127.0.0.1', port=11300, **kwargs):
+  return greenstalk.Client((url, port), use='likes')
 
 @hug.directive()
 def log(name=__name__, **kwargs):
@@ -30,19 +35,25 @@ def get_likes(response, db: _redis, post_id: hug.types.number, output=hug.output
   return {"likes": db.get(f"posts:{post_id}:likes")}
 
 # Like a post by a certain amount
-# TODO: check if user has already like post
-@hug.put("/likes/{post_id}", status=hug.falcon.HTTP_201)
+# inserts into likes jobs queue
+@hug.put("/likes/{post_id}", status=hug.falcon.HTTP_202)
 def add_likes(
   response, 
   post_id: hug.types.number,
   user_id: hug.types.number,
   amount: hug.types.number,
-  db: _redis
+  jobs: jobs
   ):
-  db.incr(f'posts:{post_id}:likes', amount)
-  db.rpush(f'users:{user_id}:likes', post_id)
-  num = db.get(f'posts:{post_id}:likes')
-  db.zadd('leaderboard', {post_id: num})
+  body = json.dumps({
+    'post_id': post_id,
+    'user_id': user_id,
+    'amount': amount
+  })
+  jobs.put(body)
+  # db.incr(f'posts:{post_id}:likes', amount)
+  # db.rpush(f'users:{user_id}:likes', post_id)
+  # num = db.get(f'posts:{post_id}:likes')
+  # db.zadd('leaderboard', {post_id: num})
 
 # list of users' likes
 @hug.get("/likes/users/{user_id}", output=hug.output_format.pretty_json)
