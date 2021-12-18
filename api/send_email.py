fdce3d8f767dc79e7c65a4cf1ffa6fbbb411354d
Justin Blanco <blancorjustin@gmail.com>
2021-12-17 19:35:44 -0800

    email worker implemented

--- api/send_email.py
+++ api/send_email.py
@@ -4,7 +4,7 @@ import json
 
 # connections
 client = greenstalk.Client(('127.0.0.1', 11300), watch='email')
-server = smtplib.SMTP('localhost')
+server = smtplib.SMTP('localhost', 1025)
 server.set_debuglevel(1)
 
 # Constants
@@ -14,4 +14,5 @@ while True:
   job = client.reserve()
   data = json.load(job.body)
   server.sendmail(FROM, data['username'], data['message'])
+  client.delete(job)
 
