b1400dc36382df35b9b8be6753971864c61c333b
Justin Blanco <blancorjustin@gmail.com>
2021-12-17 16:56:13 -0800

    poll worker referenced

--- api/posts_consumer.py
+++ api/posts_consumer.py
@@ -4,6 +4,8 @@ import sqlite_utils
 
 # Connect and consume jobs
 client = greenstalk.Client(('127.0.0.1', 11300))
+worker = greenstalk.Client(('127.0.0.1', 11300), use='polls')
+
 print("Using tube: ", client.using())
 print("Watching tube: ", client.watching())
 db = sqlite_utils.Database("./var/timelines.db")
@@ -23,5 +25,9 @@ while True:
   except Exception as e:
     print('Error adding to post database from consume')
   print("Successfully Added a new post from consume.")
+
+  # send to polls worker to check if text has a valid poll URL
+  worker.put(json.dumps[data])
+  
   # delete job
   client.delete(job)
